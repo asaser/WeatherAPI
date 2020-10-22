8cb5445fe7ee4001e6d20efb219fd08f7a638cb4
Bartlomiej Korytkowski <korytkowskibartlomiej@gmail.com>
2020-10-22 22:23:33 +0200

    edit structure

--- server/index.js
+++ server/index.js
@@ -1,57 +1,62 @@
 // console.log(process.env.API_KEY_NEW);
 
-   // API using jQuery
+// API using jQuery
 
-   $(document).ready(function () {
+$(document).ready(function () {
 
 
-       $("#textButton").click(function () {
+    const d = new Date();
 
-         var APIkey = process.env.API_KEY;
+    function nowaData() {
+        return d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+    }
 
-           const approvalText = $("#textCity").val();
+    $("#textButton").click(function () {
 
-           if (approvalText != '') {
+        //  var APIkey = process.env.API_KEY;
+        var APIkey = "882093c6b009651d06b4d050d0d05415"; //OpenWeatherMap website
 
-               $.ajax({
+        const approvalText = $("#textCity").val();
 
-                   type: 'GET',
-                   url: 'https://api.openweathermap.org/data/2.5/weather?q=' + approvalText + "&appid=" + APIkey + '&units=metric',
-                   dataType: "json",
-                   success: function (data) {
+        if (approvalText != '') {
 
-                       // sprawdzic inne URL na stroce, może to pomoże aby znależć RAIN oraz dodać różne grafiki
-                       // city
-                       $(".city_name").html(data.name + ', ' + data.sys.country);
+            $.ajax({
 
-                       // current day
+                type: 'GET',
+                url: 'https://api.openweathermap.org/data/2.5/weather?q=' + approvalText + "&appid=" + APIkey + '&units=metric',
+                dataType: "json",
+                success: function (data) {
 
-                       // temperature in city degrees C
-                       $(".temperature_forecast").html(data.main.temp + '<span> °C</span>');
+                    // sprawdzic inne URL na stroce, może to pomoże aby znależć RAIN oraz dodać różne grafiki
+                    // city
+                    $(".city_name").html('<span>City name: </span>' + data.name + ', ' + data.sys.country);
 
-                       // rain mm
-                       $(".rain_forecast").html(data.rain + '<span> mm</span>');
-                       // humiditi
-                       $(".humidity_forecast").html(data.main.humidity + '<span> %</span>');
-                       // clouds %
-                       $(".clouds_forecast").html(data.clouds.all + '<span> %</span>');
-                       // wind kn/h
-                       $(".wind_forecast").html(data.wind.speed + '<span> km/h</span>');
-                       // sky
-                       $(".sky_forecast").html(data.weather[0].description);
-                       // pressure
-                       $(".pressure_forecast").html(data.main.pressure + '<span> hPa</span>');
-                       console.log(data);
-                   }
+                    // current day
+                    $(".current_day").html('<span>Current day: </span>' + nowaData());
+                    // temperature in city degrees C
+                    $(".temperature_forecast").html('<span>Temp: </span>' + data.main.temp + '<span> °C</span>');
+                    // rain mm
+                    $(".rain_forecast").html('<span>Rain: </span>' + data.main.rain + '<span> mm</span>');
+                    // humiditi
+                    $(".humidity_forecast").html('<span>Humidity: </span>' + data.main.humidity + '<span> %</span>');
+                    // clouds %
+                    $(".clouds_forecast").html('<span>Cloudy: </span>' + data.clouds.all + '<span> %</span>');
+                    // wind kn/h
+                    $(".wind_forecast").html('<span>Windy: </span>' + data.wind.speed + '<span> km/h</span>');
+                    // sky
+                    $(".sky_forecast").html('<span>Sky: </span>' + data.weather[0].description);
+                    // pressure
+                    $(".pressure_forecast").html('<span>Pressure: </span>' + data.main.pressure + '<span> hPa</span>');
+                    //    console.log(data);
+                }
 
-                   //    error: function (e) {
-                   //    }
+                //    error: function (e) {
+                //    }
+            });
 
-               });
+        } else {
+            alert("Please write city name on text field");
+        }
+    });
 
-           } else {
-               alert("Please write city name on text field");
-           }
-       });
-
-   });
+});
